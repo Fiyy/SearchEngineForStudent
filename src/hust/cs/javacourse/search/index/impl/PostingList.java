fdbb27fc2bfbb960f48f617381422ac410d11f38
ZY <840109873@qq.com>
2020-05-10 20:49:54 +0800

    解决了PostingList序列化和反序列化的Bug，直接脑残化用列表序列化反序列化函数就可以了，不需要自己将每个节点序列化和反序列化 但是奇怪的是 测试程序结果从92 methods, 14 failed, 19 skipped, 59 passed 变为14 failed, 34 skipped, 42 passed
    不降反增？？？

--- src/hust/cs/javacourse/search/index/impl/PostingList.java
+++ src/hust/cs/javacourse/search/index/impl/PostingList.java
@@ -13,9 +13,8 @@ package hust.cs.javacourse.search.index.impl;
 import hust.cs.javacourse.search.index.AbstractPosting;
 import hust.cs.javacourse.search.index.AbstractPostingList;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -29,6 +28,11 @@ import java.util.List;
  * @since 1.0.0
  */
 public class PostingList extends AbstractPostingList {
+
+    /**
+     * 缺省构造函数
+     */
+    PostingList(){}
 
     /**
      * 添加Posting,要求不能有内容重复的posting
@@ -178,13 +182,15 @@ public class PostingList extends AbstractPostingList {
 
     /**
      * 写到二进制文件
-     *
+     * 一定要添加List的大小，否则无法读取
      * @param out :输出流对象
      */
     @Override
     public void writeObject(ObjectOutputStream out) {
-        for(int i = 0; i < list.size(); i++){
-            list.get(i).writeObject(out);
+        try {
+            out.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -195,8 +201,56 @@ public class PostingList extends AbstractPostingList {
      */
     @Override
     public void readObject(ObjectInputStream in) {
-        for(int i = 0; i < list.size(); i++){
-            list.get(i).readObject(in);
+        try {
+            list = (List<AbstractPosting>)in.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String...args)
+    {
+        PostingList postingList = new PostingList();
+        ArrayList<Integer> postions1 = new ArrayList();
+        for(int i= 1;i<5;i++)
+            postions1.add(i);
+        Posting posting1 = new Posting(1,4, postions1);
+        ArrayList<Integer> postions2 = new ArrayList();
+        postions2.add(2);postions2.add(4);postions2.add(3);postions2.add(1);
+        Posting posting2 = new Posting(2,4,postions2);
+        postingList.add(posting1);
+        postingList.add(posting2);
+
+        // 测试writeObject
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(new File("D:\\HUST\\java\\实验1\\test_postinglist.txt")));
+            postingList.writeObject(objectOutputStream);
+            for(int i = 0; i < postingList.size(); i++){
+                System.out.println(postingList.get(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 测试readObject
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(new File("D:\\HUST\\java\\实验1\\test_postinglist.txt")));
+            PostingList postingList1 = new PostingList();
+            postingList1.readObject(objectInputStream);
+            System.out.println("The test readObject is:\n" + postingList1.toString());
+            //该部分说明Posting的序列化函数没问题，问题在于PostingList的序列化
+//            try {
+//                int a = (Integer)objectInputStream.readObject();
+//                System.out.println("a is: " + a);
+//                Posting postingz = new Posting();
+//                postingz.readObject(objectInputStream);
+//                System.out.println("postingz is:\n" + postingz);
+//            } catch (ClassNotFoundException e) {
+//                e.printStackTrace();
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
